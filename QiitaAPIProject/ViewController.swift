952b6226c19cae76045ffba97595d39ea7a6a44e
Hiro-im <doraemon.for.private@gmail.com>
2022-07-14 07:08:17 +0900

    TableViewの下準備

--- QiitaAPIProject/ViewController.swift
+++ QiitaAPIProject/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let cellId = "cellId"
+    
+    let tableView: UITableView = {
+        let tv = UITableView()
+            return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.frame.size = view.frame.size
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
-
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+}
