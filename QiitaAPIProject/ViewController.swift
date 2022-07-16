56ae57140c9b4aaf248cc6b8f75092fcece639c9
Hiro-im <doraemon.for.private@gmail.com>
2022-07-16 11:10:35 +0900

    Qiitaから取得する処理まで記載

--- QiitaAPIProject/ViewController.swift
+++ QiitaAPIProject/ViewController.swift
@@ -25,6 +25,33 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        navigationItem.title = "Qiitaの記事"
+        
+        getQiitaApi()
+        
+    }
+    
+    private func getQiitaApi() {
+        guard let url = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=1") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: url) { (data,response, err) in
+            if let err = err {
+                print("情報の取得に失敗しました。：", err)
+                return
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                    print("json：", json)
+                } catch(let err) {
+                    print("情報の取得に失敗しました。：", err)
+                }
+            }
+        }
+        task.resume()
     }
 
 }
